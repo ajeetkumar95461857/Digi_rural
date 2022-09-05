3f56337b2da9344cb498581d76de5084b14554c0
Sujeet <sujeetkumariet1@gmail.com>
2022-09-06 00:11:59 +0530

    assistant

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const button=document.getElementById("button11");
+var textshow=document.getElementById("textshow");
 button.addEventListener("click",runSpeechRecognition)
 function runSpeechRecognition() {
    
@@ -21,7 +22,37 @@ function runSpeechRecognition() {
         recognition.onresult = function(event) {
             var transcript = event.results[0][0].transcript;
             output.innerHTML =transcript ;
+            const response = process(transcript);
+            speechSynthesis.speak(new SpeechSynthesisUtterance(response));
+
             output.classList.remove("hide");
         };
+       
          recognition.start();
+         
+
   }
+  function process(rawText) {
+    let text = rawText.replace(/\s/g, "");
+    text = text.toLowerCase();
+    let response = null;
+    switch(text) {
+        case "hello":
+            response = "hi sujeet"; break;
+        case "what'syourname":
+            response = "My name's ajeet.";  break;
+        case "howareyou":
+            response = "I'm good."; break;
+        case "who are you":
+            response = new Date().toLocaleTimeString(); break;
+        case "stop":
+            response = "Bye";
+            toggleBtn();
+    }
+    if (!response) {
+        window.open(`http://google.com/search?q=${rawText.replace("search", "")}`, "_blank");
+        return `I found some information for ${rawText}`;
+    }
+    return response;
+}
+ 
