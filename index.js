fe6507287ccdd707e91a50f5f2bf92d26cc866df
Sujeet <sujeetkumariet1@gmail.com>
2022-09-05 23:32:40 +0530

    add speech api

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("ajeet");
+const button=document.getElementById("button11");
+button.addEventListener("click",runSpeechRecognition())
+function runSpeechRecognition() {
+   
+    var output = document.getElementById("output");
+  
+    var action = document.getElementById("action");
+        
+        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+        var recognition = new SpeechRecognition();
+    
+        recognition.onstart = function() {
+            action.innerHTML = "<small>listening, please speak...</small>";
+        };
+        
+        recognition.onspeechend = function() {
+            action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+            recognition.stop();
+        }
+      
+        recognition.onresult = function(event) {
+            var transcript = event.results[0][0].transcript;
+            output.innerHTML =transcript ;
+            output.classList.remove("hide");
+        };
+         recognition.start();
+  }
